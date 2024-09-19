763ce39adaf00b285370eb1357c2b2b02ce1040a
Shah1901 <shaharsalan1901@gmail.com>
2024-09-19 16:13:49 +0530

    First Commit

---
+++ conversion.java
@@ -0,0 +1,8 @@
+public class conversion{
+public static void main(String[] args) {
+        //cast is used here  (byte)
+    byte b = 4;
+    b = (byte)(b*2);
+    System.out.println(b);
+}
+}
---
+++ gcxj.java
@@ -0,0 +1,5 @@
+public class gcxj {
+    public static void main(String[] args) {
+        System.out.println("hey Shah");
+    }
+}
